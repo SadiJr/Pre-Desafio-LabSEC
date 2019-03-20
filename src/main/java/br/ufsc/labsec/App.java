e9466a5cc292457b8dc0ace48dd6934c88913fa3
SadiJr <sadijacinto@gmail.com>
2019-03-20 14:02:49 -0300

    Teste 1 finalizado. O mesmo foi facilmente solucionada ao usar a biblioteca do Apache Commons

--- src/main/java/br/ufsc/labsec/App.java
+++ src/main/java/br/ufsc/labsec/App.java
@@ -1,13 +1,15 @@
 package br.ufsc.labsec;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		try {
+			System.out.println("Iniciando testes!");
+			String convertToBase64 = HexToBase64Converter.convertToBase64("49276d206b696c6c6"
+					+ "96e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d7573"
+					+ "68726f6f6d");
+			System.out.println("Resultado da conversão = " + convertToBase64);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
 }
--- src/main/java/br/ufsc/labsec/HexToBase64Converter.java
+++ src/main/java/br/ufsc/labsec/HexToBase64Converter.java
@@ -1,5 +1,12 @@
 package br.ufsc.labsec;
 
-public class HexToBase64Converter {
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
 
+public class HexToBase64Converter {
+	
+	public static String convertToBase64(String hex) throws DecoderException {
+		return new String(Base64.encodeBase64(Hex.decodeHex(hex.toCharArray())));
+	}
 }
