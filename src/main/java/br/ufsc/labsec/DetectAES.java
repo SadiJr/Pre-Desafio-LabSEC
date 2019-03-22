49fec9c1f0f8e8537f9ebabdb86e1ae8873e0f3c
SadiJr <sadijacinto@gmail.com>
2019-03-22 09:20:56 -0300

    Desafio 8 concluído. Faltando apenas o 6

--- src/main/java/br/ufsc/labsec/DetectAES.java
+++ src/main/java/br/ufsc/labsec/DetectAES.java
@@ -1,48 +1,66 @@
 package br.ufsc.labsec;
 
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.SecretKeySpec;
+import java.util.Arrays;
 
 import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
 
+/**
+ * 
+ * @author Sadi
+ * 
+ * Desafio um pouco mais complicado...
+ * Foi necessário percorrer todas as linhas, separando-as em byte[] de 16 (que são os blocos criptografados) e
+ * verificando se dois blocos eram iguais.
+ * 
+ * Como o ECB criptografa blocos de texto puro iguais em blocos de texto criptografados iguais, ele não oculta os padrões de dados.
+ * 
+ * Por isso, bastou agrupar os dados em blocos de 16 bytes e verificar os trechos repetidos. A princípio pensei em fazer um contador para
+ * verificar qual linha tinha mais repetições, mas, conforme eu ia desevolvendo (por tentativa e erro, admito), me dei conta de que 
+ * apenas uma linha estava cifrada (erro de interpretação meu novamente) e que, portanto, não era necessário tal nível de complexidade
+ *
+ */
 public class DetectAES {
-	private static SecretKeySpec key;
-	
-	public static String detect(String text) throws DecoderException, NoSuchAlgorithmException, 
-			NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-		String[] split = text.split("\n");
+
+	public static String detect(String text) throws DecoderException {
+		String result = "";
+		String[] lines = text.split("\n");
+//		int line = 1;
 		
-		for (String string : split) {
-//			System.out.println(HexToBase64Converter.convertToBase64(string));
-//			System.out.println(string);
-//			byte[] decodeBase64 = Base64.decodeBase64(HexToBase64Converter.convertToBase64(string));
-			byte[] decodeBase64 = string.getBytes();
-			try {
-				for (int i = 0; i < decodeBase64.length; i++) {
-					int z = 0;
-					byte[] k = new byte[16];
-					while(z < 16) {
-						k[z] = decodeBase64[z];
-						z++;
-					}
-					key = new SecretKeySpec(k, "AES");
-					cipher.init(Cipher.DECRYPT_MODE, key);
-					System.out.println(new String(cipher.doFinal(HexToBase64Converter.convertToBase64(string).getBytes())));
-				}
-			} catch (Exception e) {
-				continue;
+		for (String l : lines) {
+			byte[] decodeHex = Hex.decodeHex(l.toCharArray());
+			byte[][] data = new byte[decodeHex.length / 16][16];
+			int pos = 0;
+			
+			for (int i = 0; i < decodeHex.length / 16; i++) {
+				for (int j = 0; j < 16; j++)
+					data[i][j] = decodeHex[pos++];
 			}
-//			System.out.println(new String(decodeBase64));
+			
+			boolean find = false;
+			for (int i = 0; i < data.length; i++) {
+				
+				//TODO Otimizar método depois (3 FORs aninhados de novo).
+				//Ver outros "TODO" espalhados pelo projeto.
+				if (find)
+					break;
+				
+				for (int j = 0; j < data.length; j++) {
+					if (i == j)
+						continue;
+					if (Arrays.equals(data[i], data[j])) {
+//						System.out.println("i = " + new String(Hex.encodeHex(new String(data[i]).getBytes())));
+//						System.out.println("j = " + new String(Hex.encodeHex(new String(data[j]).getBytes())));
+//						System.out.println("A linha " + line + " deve ser ECB");
+//						System.out.println(l);
+						result = l;
+						find = true;
+						break;
+					}
+				}
+			}
+//			line++;
 		}
-		
-		return "";
+		return result;
 	}
 }
--- src/test/java/br/ufsc/labsec/AppTest.java
+++ src/test/java/br/ufsc/labsec/AppTest.java
@@ -9,21 +9,19 @@ import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class AppTest {
-	
+
 	@Test
 	public void shouldConvertHexStringToBase64Correctly() {
 		try {
 			String convertToBase64 = HexToBase64Converter.convertToBase64("49276d206b696c6c6"
-				+ "96e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d7573"
-				+ "68726f6f6d");
+					+ "96e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d7573" + "68726f6f6d");
 			Assert.assertEquals("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t", convertToBase64);
 		} catch (Exception e) {
 			fail("Um erro inesperado ocorreu");
 		}
 	}
-	
+
 	@Test
 	public void shouldReturnEncryptedStringWithXOR() {
 		try {
@@ -34,11 +32,12 @@ public class AppTest {
 			fail("Um erro inesperado ocorreu");
 		}
 	}
-	
+
 	@Test
 	public void shouldDescryptMessage() {
 		try {
-			Map<Character, Map<String, Double>> decryptMessage = SingleXORCipher.decryptMessage("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
+			Map<Character, Map<String, Double>> decryptMessage = SingleXORCipher
+					.decryptMessage("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
 			char key = (Character) decryptMessage.keySet().toArray()[0];
 			Assert.assertEquals('X', key);
 			String message = (String) decryptMessage.get(key).keySet().toArray()[0];
@@ -47,7 +46,7 @@ public class AppTest {
 			fail("Um erro inesperado ocorreu");
 		}
 	}
-	
+
 	@Test
 	public void shouldDescryptFile() {
 		try {
@@ -60,19 +59,19 @@ public class AppTest {
 			fail("Um erro inesperado ocorreu");
 		}
 	}
-	
+
 	@Test
 	public void shouldEncryptTextWithRepeatingXOR() {
 		try {
-			String encrypt = RepeatingXOREncryptor.encrypt("Burning 'em, if you ain't quick and nimble\n" + 
-					"I go crazy when I hear a cymbal");
+			String encrypt = RepeatingXOREncryptor
+					.encrypt("Burning 'em, if you ain't quick and nimble\n" + "I go crazy when I hear a cymbal");
 			Assert.assertEquals("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"
 					+ "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f", encrypt);
 		} catch (Exception e) {
 			fail("Um erro inesperado ocorreu");
 		}
 	}
-	
+
 	@Test
 	public void shouldDecryptBase64WithAESFile() {
 		try {
@@ -83,86 +82,64 @@ public class AppTest {
 			fail("Um erro inesperado ocorreu");
 		}
 	}
-	
+
+	@Test
+	public void shouldReturnAESEncryptedString() {
+		try {
+			String file = new String(FileUtils.readFileToByteArray(new File("src/main/resources/challenge_08.txt")));
+			String detect = DetectAES.detect(file);
+			Assert.assertEquals("d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d1"
+					+ "1b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4f"
+					+ "d5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f"
+					+ "6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a",
+					detect);
+		} catch (Exception e) {
+			fail("Um erro inesperado ocorreu");
+		}
+	}
+
 	private String aesFile() {
-		return "I'm back and I'm ringin' the bell \n" + 
-				"A rockin' on the mike while the fly girls yell \n" + 
-				"In ecstasy in the back of me \n" + 
-				"Well that's my DJ Deshay cuttin' all them Z's \n" + 
-				"Hittin' hard and the girlies goin' crazy \n" + 
-				"Vanilla's on the mike, man I'm not lazy. \n" + 
-				"\n" + 
-				"I'm lettin' my drug kick in \n" + 
-				"It controls my mouth and I begin \n" + 
-				"To just let it flow, let my concepts go \n" + 
-				"My posse's to the side yellin', Go Vanilla Go! \n" + 
-				"\n" + 
-				"Smooth 'cause that's the way I will be \n" + 
-				"And if you don't give a damn, then \n" + 
-				"Why you starin' at me \n" + 
-				"So get off 'cause I control the stage \n" + 
-				"There's no dissin' allowed \n" + 
-				"I'm in my own phase \n" + 
-				"The girlies sa y they love me and that is ok \n" + 
-				"And I can dance better than any kid n' play \n" + 
-				"\n" + 
-				"Stage 2 -- Yea the one ya' wanna listen to \n" + 
-				"It's off my head so let the beat play through \n" + 
-				"So I can funk it up and make it sound good \n" + 
-				"1-2-3 Yo -- Knock on some wood \n" + 
-				"For good luck, I like my rhymes atrocious \n" + 
-				"Supercalafragilisticexpialidocious \n" + 
-				"I'm an effect and that you can bet \n" + 
-				"I can take a fly girl and make her wet. \n" + 
-				"\n" + 
-				"I'm like Samson -- Samson to Delilah \n" + 
-				"There's no denyin', You can try to hang \n" + 
-				"But you'll keep tryin' to get my style \n" + 
-				"Over and over, practice makes perfect \n" + 
-				"But not if you're a loafer. \n" + 
-				"\n" + 
-				"You'll get nowhere, no place, no time, no girls \n" + 
-				"Soon -- Oh my God, homebody, you probably eat \n" + 
-				"Spaghetti with a spoon! Come on and say it! \n" + 
-				"\n" + 
-				"VIP. Vanilla Ice yep, yep, I'm comin' hard like a rhino \n" + 
-				"Intoxicating so you stagger like a wino \n" + 
-				"So punks stop trying and girl stop cryin' \n" + 
-				"Vanilla Ice is sellin' and you people are buyin' \n" + 
-				"'Cause why the freaks are jockin' like Crazy Glue \n" + 
-				"Movin' and groovin' trying to sing along \n" + 
-				"All through the ghetto groovin' this here song \n" + 
-				"Now you're amazed by the VIP posse. \n" + 
-				"\n" + 
-				"Steppin' so hard like a German Nazi \n" + 
-				"Startled by the bases hittin' ground \n" + 
-				"There's no trippin' on mine, I'm just gettin' down \n" + 
-				"Sparkamatic, I'm hangin' tight like a fanatic \n" + 
-				"You trapped me once and I thought that \n" + 
-				"You might have it \n" + 
-				"So step down and lend me your ear \n" + 
-				"'89 in my time! You, '90 is my year. \n" + 
-				"\n" + 
-				"You're weakenin' fast, YO! and I can tell it \n" + 
-				"Your body's gettin' hot, so, so I can smell it \n" + 
-				"So don't be mad and don't be sad \n" + 
-				"'Cause the lyrics belong to ICE, You can call me Dad \n" + 
-				"You're pitchin' a fit, so step back and endure \n" + 
-				"Let the witch doctor, Ice, do the dance to cure \n" + 
-				"So come up close and don't be square \n" + 
-				"You wanna battle me -- Anytime, anywhere \n" + 
-				"\n" + 
-				"You thought that I was weak, Boy, you're dead wrong \n" + 
-				"So come on, everybody and sing this song \n" + 
-				"\n" + 
-				"Say -- Play that funky music Say, go white boy, go white boy go \n" + 
-				"play that funky music Go white boy, go white boy, go \n" + 
-				"Lay down and boogie and play that funky music till you die. \n" + 
-				"\n" + 
-				"Play that funky music Come on, Come on, let me hear \n" + 
-				"Play that funky music white boy you say it, say it \n" + 
-				"Play that funky music A little louder now \n" + 
-				"Play that funky music, white boy Come on, Come on, Come on \n" + 
-				"Play that funky music";
+		return "I'm back and I'm ringin' the bell \n" + "A rockin' on the mike while the fly girls yell \n"
+				+ "In ecstasy in the back of me \n" + "Well that's my DJ Deshay cuttin' all them Z's \n"
+				+ "Hittin' hard and the girlies goin' crazy \n" + "Vanilla's on the mike, man I'm not lazy. \n" + "\n"
+				+ "I'm lettin' my drug kick in \n" + "It controls my mouth and I begin \n"
+				+ "To just let it flow, let my concepts go \n" + "My posse's to the side yellin', Go Vanilla Go! \n"
+				+ "\n" + "Smooth 'cause that's the way I will be \n" + "And if you don't give a damn, then \n"
+				+ "Why you starin' at me \n" + "So get off 'cause I control the stage \n"
+				+ "There's no dissin' allowed \n" + "I'm in my own phase \n"
+				+ "The girlies sa y they love me and that is ok \n" + "And I can dance better than any kid n' play \n"
+				+ "\n" + "Stage 2 -- Yea the one ya' wanna listen to \n"
+				+ "It's off my head so let the beat play through \n" + "So I can funk it up and make it sound good \n"
+				+ "1-2-3 Yo -- Knock on some wood \n" + "For good luck, I like my rhymes atrocious \n"
+				+ "Supercalafragilisticexpialidocious \n" + "I'm an effect and that you can bet \n"
+				+ "I can take a fly girl and make her wet. \n" + "\n" + "I'm like Samson -- Samson to Delilah \n"
+				+ "There's no denyin', You can try to hang \n" + "But you'll keep tryin' to get my style \n"
+				+ "Over and over, practice makes perfect \n" + "But not if you're a loafer. \n" + "\n"
+				+ "You'll get nowhere, no place, no time, no girls \n"
+				+ "Soon -- Oh my God, homebody, you probably eat \n" + "Spaghetti with a spoon! Come on and say it! \n"
+				+ "\n" + "VIP. Vanilla Ice yep, yep, I'm comin' hard like a rhino \n"
+				+ "Intoxicating so you stagger like a wino \n" + "So punks stop trying and girl stop cryin' \n"
+				+ "Vanilla Ice is sellin' and you people are buyin' \n"
+				+ "'Cause why the freaks are jockin' like Crazy Glue \n" + "Movin' and groovin' trying to sing along \n"
+				+ "All through the ghetto groovin' this here song \n" + "Now you're amazed by the VIP posse. \n" + "\n"
+				+ "Steppin' so hard like a German Nazi \n" + "Startled by the bases hittin' ground \n"
+				+ "There's no trippin' on mine, I'm just gettin' down \n"
+				+ "Sparkamatic, I'm hangin' tight like a fanatic \n" + "You trapped me once and I thought that \n"
+				+ "You might have it \n" + "So step down and lend me your ear \n"
+				+ "'89 in my time! You, '90 is my year. \n" + "\n" + "You're weakenin' fast, YO! and I can tell it \n"
+				+ "Your body's gettin' hot, so, so I can smell it \n" + "So don't be mad and don't be sad \n"
+				+ "'Cause the lyrics belong to ICE, You can call me Dad \n"
+				+ "You're pitchin' a fit, so step back and endure \n"
+				+ "Let the witch doctor, Ice, do the dance to cure \n" + "So come up close and don't be square \n"
+				+ "You wanna battle me -- Anytime, anywhere \n" + "\n"
+				+ "You thought that I was weak, Boy, you're dead wrong \n"
+				+ "So come on, everybody and sing this song \n" + "\n"
+				+ "Say -- Play that funky music Say, go white boy, go white boy go \n"
+				+ "play that funky music Go white boy, go white boy, go \n"
+				+ "Lay down and boogie and play that funky music till you die. \n" + "\n"
+				+ "Play that funky music Come on, Come on, let me hear \n"
+				+ "Play that funky music white boy you say it, say it \n"
+				+ "Play that funky music A little louder now \n"
+				+ "Play that funky music, white boy Come on, Come on, Come on \n" + "Play that funky music";
 	}
 }
